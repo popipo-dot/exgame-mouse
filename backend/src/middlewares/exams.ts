7a198d35353b66a286525febbf3d98b2b8a9860b
Alex Pezzini <alex.pezzini@intesys.it>
2025-10-22 17:27:31 +0200

    Add exams middleware for validation of exam registration

---
+++ backend/src/middlewares/exams.ts
@@ -0,0 +1,30 @@
+import { Context, Next } from "koa";
+import { subscriptions } from "../mocks/subscriptions";
+export const examsMiddleware = async (ctx: Context, next: Next) => {
+  const { exam_id, student_id } = ctx.request.body;
+
+  if (!exam_id) {
+    ctx.status = 400;
+    ctx.body = { error: "Parametro 'exam_id' mancante" };
+    return;
+  }
+
+  if (!student_id) {
+    ctx.status = 400;
+    ctx.body = { error: "Parametro 'student_id' mancante" };
+    return;
+  }
+
+  // Controlla se lo studente ha già registrato l'esame
+  const studentYetRegistered = subscriptions.some(
+    (sub) => sub.exam_id === exam_id && sub.student_id === student_id,
+  );
+
+  if (studentYetRegistered) {
+    ctx.status = 400;
+    ctx.body = { error: "Esame già registrato da questo studente" };
+    return;
+  }
+
+  await next();
+};
